d7459b4717365e23fb83c6170c992db29bb9fe0e
jwcodeuk <jonathan@iotechsys.com>
2025-12-17 11:30:24 +0000

    add ekuiper support

--- tutorial/scripts/ekupier_rule.sh
+++ tutorial/scripts/ekupier_rule.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
-curl -X POST http://localhost:59720/rules -H 'Content-Type: application/json' -d '{                                           
-  "id": "rule",                                                                                                                    
-  "sql": "SELECT AirPressure, CASE WHEN AirPressure > 2999 THEN true ELSE false END as setting FROM demo WHERE meta(deviceName) = \"Controller-2\" AND meta(sourceName) = \"AirPressure\"",
+curl -X POST http://localhost:59720/rules -H 'Content-Type: application/json' -d '{
+  "id": "rule",
+  "sql": "SELECT avg(AirPressure) as avgPressure, CASE WHEN avg(AirPressure) > 2999 THEN true ELSE false END as setting FROM demo WHERE meta(deviceName) IN (\"Controller-0\", \"Controller-1\", \"Controller-2\") GROUP BY TUMBLINGWINDOW(ss, 10)",
   "actions": [
     {
       "mqtt": {
-        "server": "tcp://edgex-mqtt-broker:1883",
+        "server": "tcp://mqtt-broker:1883",
         "topic": "xrt/mqtt/input",
-        "clientId": "controller0_001",
-        "dataTemplate": "{\"setting\":{{.setting}}, \"source\" : \"ekuiper\"}",
+        "clientId": "controller_avg",
+        "dataTemplate": "{\"avgPressure\":{{.avgPressure}}, \"setting\":{{.setting}}, \"source\":\"ekuiper\"}",
         "sendSingle": true
       }
     },
